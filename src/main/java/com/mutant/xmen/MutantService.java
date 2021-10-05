7c4fc14b0411a08837094e88ed537e278bea7ef8
Jackson Asprilla Blandon <yayokalonji@Jacksons-MacBook-Pro.local>
2021-10-05 16:48:23 -0500

    Return true o false functions

--- src/main/java/com/mutant/xmen/MutantService.java
+++ src/main/java/com/mutant/xmen/MutantService.java
@@ -6,7 +6,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class MutantService {
 
+    private static final String A = "AAAA";
+    private static final String T = "TTTT";
+    private static final String C = "CCCC";
+    private static final String G = "GGGG";
+
     public boolean isMutant(RequestDTO requestDTO) {
-        return true;
+        return requestDTO.getDna().stream().anyMatch(s -> s.contains(A) || s.contains(T) || s.contains(C) || s.contains(G));
     }
 }
