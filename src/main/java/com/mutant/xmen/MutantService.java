12a89cd494fe70fa71125b082ddf4f5001b72f78
Jackson Asprilla Blandon <yayokalonji@Jacksons-MacBook-Pro.local>
2021-10-06 11:25:41 -0500

    Add method search

--- src/main/java/com/mutant/xmen/MutantService.java
+++ src/main/java/com/mutant/xmen/MutantService.java
@@ -12,6 +12,32 @@ public class MutantService {
     private static final String G = "GGGG";
 
     public boolean isMutant(RequestDTO requestDTO) {
-        return requestDTO.getDna().stream().anyMatch(s -> s.contains(A) || s.contains(T) || s.contains(C) || s.contains(G));
+        if (searchVertical(requestDTO)) {
+            return true;
+        } else {
+            return searchHorizontal(requestDTO);
+        }
+    }
+
+    private boolean searchHorizontal(RequestDTO requestDTO) {
+        return requestDTO.getDna().stream().anyMatch(
+                s -> s.toUpperCase().contains(A) || s.toUpperCase().contains(T) || s.toUpperCase().contains(C) || s.toUpperCase().contains(G));
+    }
+
+    private boolean searchVertical(RequestDTO requestDTO) {
+        StringBuilder chain = new StringBuilder();
+        boolean result = false;
+        for (int i = 0; i < requestDTO.getDna().size(); i++) {
+            for (int j = 0; j < requestDTO.getDna().size(); j++) {
+                chain.append(requestDTO.getDna().get(j).charAt(i));
+            }
+            result = chain.toString().toUpperCase().contains(A) || chain.toString().toUpperCase().contains(T)
+                    || chain.toString().toUpperCase().contains(C) || chain.toString().toUpperCase().contains(G);
+            chain.setLength(0);
+            if (result) {
+                break;
+            }
+        }
+        return result;
     }
 }
